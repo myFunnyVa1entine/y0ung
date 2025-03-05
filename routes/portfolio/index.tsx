39b6f0f9af89563fe9d09bf788a5e6afe2b5014d
myFunnyVa1entine <brilliant2102@gmail.com>
2025-03-05 13:48:10 +0900

    Style: Fixed poster image height

--- routes/portfolio/index.tsx
+++ routes/portfolio/index.tsx
@@ -8,7 +8,7 @@ function Articles({ href }: { href: string }) {
 
 export default function portfolio() {
   return (
-    <section class="mx-10 md:max-w-[665px] md:mx-auto text-white min-h-full md:grid md:grid-cols-2 md:gap-4">
+    <section class="mx-10 md:max-w-[665px] md:mx-auto text-white md:grid md:grid-cols-2 md:gap-4">
       <Articles href="https://pray.deno.dev/poster.gif" />
       <Articles href="portfolio/paperground.png" />
       <p class="pt-20 text-3xl w-full col-span-2 text-center md:h-[36vh]">
