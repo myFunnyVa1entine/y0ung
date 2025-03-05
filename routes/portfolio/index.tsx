12faf2f288e27b097aff49583d28265e194612bc
myFunnyVa1entine <brilliant2102@gmail.com>
2025-03-05 11:59:51 +0900

    Docs : Add portfolio page

--- routes/portfolio/index.tsx
+++ routes/portfolio/index.tsx
@@ -1,6 +1,19 @@
+function Articles({ href }: { href: string }) {
+  return (
+    <article class="w-full">
+      <img class="h-full object-fill" src={href} alt="home image" />
+    </article>
+  );
+}
+
 export default function portfolio() {
   return (
-    <section class="mx-10 md:max-w-[665px] md:mx-auto text-white min-h-full">
+    <section class="mx-10 md:max-w-[665px] md:mx-auto text-white min-h-full grid grid-cols-2 gap-4 p-4">
+      <Articles href="https://pray.deno.dev/poster.gif" />
+      <Articles href="portfolio/paperground.png" />
+      <p class="h-[36vh] pt-20 text-3xl w-full col-span-2 text-center">
+        coming soon...
+      </p>
     </section>
   );
 }
